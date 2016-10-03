07d274a327529e825570af1b604fc173aeec6755
roelveldhuizen <roel@inesta.nl>
2016-10-03 19:34:14 +0200

    Init

---
+++ travis-test.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name:     Travis Test
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     travis-test
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Travis_Test
+ */
