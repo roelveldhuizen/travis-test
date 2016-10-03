b07f1650db10149e3a5e59b6bc5be39e7b9d948b
roelveldhuizen <roel@inesta.nl>
2016-10-03 19:39:05 +0200

    Extra test

--- tests/test-sample.php
+++ tests/test-sample.php
@@ -10,11 +10,19 @@
  */
 class SampleTest extends WP_UnitTestCase {
 
-	/**
-	 * A single example test.
-	 */
-	function test_sample() {
-		// Replace this with some actual testing code.
-		$this->assertTrue( true );
-	}
+    /**
+     * A single example test.
+     */
+    function test_sample() {
+        // Replace this with some actual testing code.
+        $this->assertTrue( true );
+    }
+
+    /**
+     * A single example test.
+     */
+    function test_sample2() {
+        // Replace this with some actual testing code.
+        $this->assertTrue( true );
+    }
 }
