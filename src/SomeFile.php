23f71cd1404494d866353bc29ca1b9e263e92701
roelveldhuizen <roel@inesta.nl>
2016-10-05 12:22:05 +0200

    Untested bad function

--- src/SomeFile.php
+++ src/SomeFile.php
@@ -6,6 +6,20 @@ function use_a_wordpress_function() {
     return count( $posts );
 }
 
+function some_ugly_function(){
+    $_SESSION['start'] = 'Yes';
+
+    return 0;
+}
+
+function use_global(){
+    global $user;
+
+    if(isset($user)){
+        return true;
+    }
+}
+
 function some_untested_function() {
     $i = 0;
     for ( $i = 0; $i < 100; $i ++ ) {
